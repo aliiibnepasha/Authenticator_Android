660df92754fbff16a547fe6b1dfaa139be664b88
Husnain Rafique Veroke <husnainrafiqueveroke@gmail.com>
2025-01-29 19:11:04 +0500

    build v=1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 26
         //noinspection EditedTargetSdkVersion,OldTargetApi
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.1.4"
+        versionCode = 7
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
